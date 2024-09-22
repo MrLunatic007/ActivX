0dde8072a54598dffa1b7a4ed7249784c988d864
MrLunatic007 <mrlunatic.davis.com>
2024-09-22 19:24:28 +0100

    modified:   ActivX/__pycache__/settings.cpython-311.pyc modified:   ActivX/__pycache__/urls.cpython-311.pyc 	modified:   ActivX/settings.py 	modified:   ActivX/urls.py

--- ActivX/urls.py
+++ ActivX/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("ActivX.urls")),
+    path('', include("ActivX_app.urls")),
     path('admin/', admin.site.urls),
 ]
