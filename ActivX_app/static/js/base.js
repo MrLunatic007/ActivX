fde6919331d9a6be151be5498c4cf1ab4115c6f1
MrLunatic007 <mrlunatic.davis.com>
2024-09-22 19:54:39 +0100

    modified:   ActivX_app/static/css/base.css 	modified:   ActivX_app/static/js/base.js 	modified:   ActivX_app/templates/base.html

--- ActivX_app/static/js/base.js
+++ ActivX_app/static/js/base.js
@@ -1,19 +1,22 @@
-$(document).ready(function() {
+const sidebarBox = document.querySelector("#box"),
+	sidebarBtn = document.querySelector("#btn"),
+	pageWrapper = document.querySelector("#page-wrapper");
 
-    function toggleSidebar() {
-      $(".button").toggleClass("active");
-      $("main").toggleClass("move-to-left");
-      $(".sidebar-item").toggleClass("active");
-    }
-  
-    $(".button").on("click tap", function() {
-      toggleSidebar();
-    });
-  
-    $(document).keyup(function(e) {
-      if (e.keyCode === 27) {
-        toggleSidebar();
-      }
-    });
-  
-  });
+sidebarBtn.addEventListener("click", (event) => {
+	sidebarBtn.classList.toggle("active");
+	sidebarBox.classList.toggle("active");
+});
+
+pageWrapper.addEventListener("click", (event) => {
+	if (sidebarBox.classList.contains("active")) {
+		sidebarBtn.classList.remove("active");
+		sidebarBox.classList.remove("active");
+	}
+});
+
+window.addEventListener("keydown", (event) => {
+	if (sidebarBox.classList.contains("active") && event.keyCode === 27) {
+		sidebarBtn.classList.remove("active");
+		sidebarBox.classList.remove("active");
+	}
+});
